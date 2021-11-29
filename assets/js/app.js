a616082b9ba95c75eaba166f93eb862d4dced962
rr1000 <rrichrs@gmail.com>
2021-11-28 19:58:41 -0800

    fix: removed jquery, added ability to save dm

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,29 @@
-$(function() {
-    $(".switch").click(function() {
-        console.log("suh");
-        $("body").toggleClass("dark-mode");
+let darkMode = localStorage.getItem('darkMode');
+const darkModeToggle = document.querySelector('.switch');
+const enableDarkMode = () => {
+    document.body.classList.add('dark-mode');
+    localStorage.setItem('darkMode', 'enabled');
+}
+const disableDarkMode = () => {
+    document.body.classList.remove('dark-mode');
+    localStorage.setItem('darkMode', null);
+}
+
+if(darkMode === 'enabled'){
+    enableDarkMode();
+}
+
+if(darkModeToggle){
+    darkModeToggle.addEventListener('click', () => {
+        darkMode = localStorage.getItem('darkMode');
+        if (darkMode !== 'enabled'){
+            enableDarkMode();
+            console.log(darkMode);
+        }else{
+            disableDarkMode();
+            console.log(darkMode);
+        }
     });
-});
+}else{
+    console.log('The page is not ready');
+}
